1b4fae4b5c4058ac85189c1c97445d62ddf03322
PhivosJiar <paulyuancho@gmail.com>
2023-02-20 16:24:28 +0800

    feat: Add HttpStatusEnum for managing HTTP status codes
    Create HttpStatusEnum to store HTTP status codes as constants for easier use throughout the project.
    Define common status codes as properties in the HttpStatusEnum file, and import it into relevant files to use as needed.

---
+++ src/enum/http.ts
@@ -0,0 +1,15 @@
+export enum HttpStatusEnum {
+  OK = 200,
+  MovedPermanently = 301,
+  MovedTemporarily = 302,
+  NotModified = 304,
+  BadReqest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  InternalServerError = 500,
+  BadGateway = 502,
+  ServiceUnavailable = 503,
+  GatewayTimeout = 504,
+}
