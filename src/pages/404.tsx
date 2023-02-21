0337cafee7edc72ee59f80c45ab44fa6b3024f49
PhivosJiar <paulyuancho@gmail.com>
2023-02-21 09:43:14 +0800

    feat: Add custom 404 Not Found page
    Add a link to go back to the homepage.

---
+++ src/pages/404.tsx
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import React from 'react';
+
+import styles from '@/styles/NotFound.module.scss';
+
+function CustomNotFound() {
+  const stars = Array.from({ length: 30 }, (_, index) => index);
+  const notFound = '404 Page Not Found.';
+  const backToHome = 'back to home';
+  return (
+    <div className={styles.container}>
+      <div className={styles.sky}>
+        {stars.map((index) => (
+          <div key={index} className={styles.star}></div>
+        ))}
+      </div>
+
+      <div className={styles.textcontainer}>
+        <h1>
+          {/* 404 not found text */}
+          {[...notFound].map((char, index) => (
+            <span key={index} className={styles.text}>
+              {char.trim() || ' '}
+            </span>
+          ))}
+        </h1>
+
+        {/* back to home text */}
+        <Link href="/">
+          {[...backToHome].map((char, index) => (
+            <span key={index} className={styles.text}>
+              {char.trim() || ' '}
+            </span>
+          ))}
+        </Link>
+      </div>
+
+      {/* astronaut */}
+      <div className={styles.boi}>
+        <div className={styles.rightleg}></div>
+        <div className={styles.leftleg}></div>
+        <div className={styles.backpack}></div>
+        <div className={styles.belly}></div>
+        <div className={styles.eye}></div>
+        <div className={styles.leftleg}></div>
+      </div>
+
+      <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg">
+        <filter id="inset" x="-50%" y="-50%" width="200%" height="200%">
+          <feFlood floodColor="black" result="outside-color" />
+          <feMorphology in="SourceAlpha" operator="dilate" radius="3.5" />
+          <feComposite
+            in="outside-color"
+            operator="in"
+            result="outside-stroke"
+          />
+          <feFlood floodColor="#0c9fc4" result="inside-color" />
+          <feComposite in2="SourceAlpha" operator="in" result="inside-stroke" />
+          <feMerge>
+            <feMergeNode in="outside-stroke" />
+            <feMergeNode in="inside-stroke" />
+          </feMerge>
+        </filter>
+      </svg>
+    </div>
+  );
+}
+
+export default CustomNotFound;
