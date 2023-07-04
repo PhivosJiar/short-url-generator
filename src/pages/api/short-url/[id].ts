1aae01fb9a4efa13f1fb4a0ca70e2bbc1153eb40
PhivosJiar <paulyuancho@gmail.com>
2023-07-04 16:07:16 +0800

    wip: wip

--- src/pages/api/short-url/[id].ts
+++ src/pages/api/short-url/[id].ts
@@ -35,6 +35,7 @@ export default async function handler(
   let body: ResBody = { message: 'ok' };
   let status: number = HttpStatusEnum.OK;
   try {
+    console.log('test');
     const { id } = req.query;
 
     if (!id) {
